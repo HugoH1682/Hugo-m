454a18adf478afe1672d62072c7f7bf4ae48d521
HugoH1682 <33664012+HugoH1682@users.noreply.github.com>
2019-02-21 14:10:55 -0600

    Comentarios JS

--- MagmaLabs/main.js
+++ MagmaLabs/main.js
@@ -1,4 +1,5 @@
- $(function() {
+//SIMPLE FUNCIÓN PARA QUE CON UNA AL DAR CLICK VAYA A MI INDICADOR DIVVACIO USANDO LA POSICIÓN QUE OBTIENE LA BUILT-IN FUNCION OFFSET. 
+$(function() {
     $('.scroll-down').click (function() {
       $('html, body').animate({scrollTop: $('div.divvacio').offset().top });
    
